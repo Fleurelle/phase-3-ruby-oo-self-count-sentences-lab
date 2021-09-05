9a43e59039350145af949ac57d6d90db2b0cd728
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2021-09-05 16:26:37 -0400

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,8 @@ class String
   end
 
   def count_sentences
-    binding.pry
+    # binding.pry
+    # funky notations in parentheses is REGEX - look up
+    self.split(/\?|!|\./).select{|val| val.strip.length > 0}.count
   end
 end
