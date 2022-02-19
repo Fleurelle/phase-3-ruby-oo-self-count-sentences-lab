40c2570850637ad3bfb9e12746fcc8cc3a5ddb63
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-19 16:46:52 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # binding.pry
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # LOGIC: Check if the string includes (.), (?), or (!). If so, split the string. Lastly, count the number of end characters. 
+    binding.pry
+    self.end_with?("!", "?", ".").split.count
   end
 end
