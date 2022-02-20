ff2b80d1b2784f5e284ba6c4c1baca7ed3cba959
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-19 20:28:51 -0500

    commit tree 851ca5b84afa1772a8f10d85e6ce73f178e6c23f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,7 @@ class String
   end
 
   def count_sentences
-    # LOGIC: Check if the string includes (.), (?), or (!). If so, split the string. Lastly, count the number of end characters. 
-    # binding.pry
-    self.end_with?("!", "?", ".").split(' ')
+    # self.split(/[.!?]/).count
+    (self.split(/[.!?]/) - [""]).count 
   end
 end
